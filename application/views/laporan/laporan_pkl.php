2fd8dc716b04b0249b1a0669056eb99ec38a938d
Sabrang-hub <sabrangsham34@gmail.com>
2023-02-28 23:39:30 +0800

    s

--- application/views/laporan/laporan_pkl.php
+++ application/views/laporan/laporan_pkl.php
@@ -268,7 +268,7 @@
                 <br>
             </td>
             <td align="left" valign=top colspan="2">
-                <?= strtoupper(nama('m_industri', 'kab_kota', ['id', $data['industri_id']])) ?>, <?= strtoupper(tanggal(date('Y-m-d'))) ?>
+                <?= nama('m_industri', 'kab_kota', ['id', $data['industri_id']]) ?>, <?= tanggal(date('Y-m-d')) ?>
             </td>
         </tr>
         <tr>
