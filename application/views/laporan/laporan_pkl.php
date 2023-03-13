9027c0cdaa2f563be874688b62f8bd5b3fe3daff
Sabrang-hub <sabrangsham34@gmail.com>
2023-03-13 21:54:25 +0800

    s

--- application/views/laporan/laporan_pkl.php
+++ application/views/laporan/laporan_pkl.php
@@ -260,7 +260,7 @@
     <table cellspacing="0" border="0" cellpadding="4" style="border-collapse: collapse; width:100%">
         <tr>
             <td align="left" valign=top>
-                CATATAN :
+                CATATAN : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </td>
             <td align="left" valign=top>
                 <br>
